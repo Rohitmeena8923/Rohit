bbdc34df67ee26362b1f4ddd3584bfd5ecf228eb
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-04-29 11:07:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     ADMIN = '6414266397'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
     DB_URL = "mongodb+srv://Rohitmeena64:Ajmeer234590577@cluster0.mae8oyn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    DB_NAME = "Crack_Jack"
+    DB_NAME = "Rohitmeena64"
     TXT_LOG = -1002294532244
     AUTH_LOG = -1002294532244
     HIT_LOG = -1002294532244
