b9ab06917eb288f47105cbe2abe9259131979c5e
Rohitmeena8923 <157653731+Rohitmeena8923@users.noreply.github.com>
2025-04-29 10:54:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Rohit
+## Deploy To Heroku
+
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?button-url=https://github.com/&template=https://github.com/edit/here)
---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config(object):
+    BOT_TOKEN = "8012231272:AAFITViiSvq-2hXqbCaS4VBwHahNsim-woE"
+    API_ID = 12475131
+    API_HASH = "719171e38be5a1f500613837b79c536f"
+    ADMIN = '6530997270'.split(',')
+    ADMIN_ID = [int(id) for id in ADMIN]
+    DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_NAME = "Crack_Jack"
+    TXT_LOG = -1002294532244
+    AUTH_LOG = -1002294532244
+    HIT_LOG = -1002294532244
+    DRM_DUMP = -1002294532244
+    CHANNEL = --1002487777733
+    CH_URL = "https://t.me/justfortestinggwak"
+    OWNER = "https://t.me/mrcraacker"
+    THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
+    HOST = "https://drm-api-five.vercel.app"
